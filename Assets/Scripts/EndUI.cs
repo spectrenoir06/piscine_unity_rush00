89a36052358da1837b392947a2ecb30447f8c623
alelievr <alelievr@student.42.fr>
2016-04-10 19:56:45 +0200

    titleScreen

---
+++ Assets/Scripts/EndUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void onBackToMenuPressed() {
+
+	}
+
+	public void onRestartPressed() { 
+
+	}
+}
---
+++ Assets/Scripts/TItleScreen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TItleScreen : MonoBehaviour {
+
+	public GameObject	header;
+	public GameObject	effelImage;
+
+	private float		rotation = 0.5F;
+	private float		rotateDest = 15;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+/*		header.transform.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(header.transform.eulerAngles.z, rotateDest + 360, 0.02F));
+		if ((header.transform.eulerAngles.z > 14 && header.transform.eulerAngles.z < 180) || (header.transform.eulerAngles.z > 180 && header.transform.eulerAngles.z < 360 - 14))
+			rotateDest = -rotateDest;*/
+	}
+
+	public void onExitPressed() {
+		Application.Quit();
+	}
+
+	public void onStartPressed() {
+		Application.LoadLevel("level1");
+	}
+}
