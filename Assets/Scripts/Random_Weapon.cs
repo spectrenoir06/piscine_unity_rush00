465262bac52f5c27c8f42400a0fe6cb4c9912ff3
Spectre <antoine@doussaud.org>
2016-04-10 13:41:02 +0200

    random

---
+++ Assets/Scripts/Random_Weapon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Random_Weapon : MonoBehaviour {
+
+	public List<GameObject>		weapons = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Instantiate (weapons [Random.Range (0, weapons.Count)], transform.position, Quaternion.identity);
+		GameObject.Destroy(gameObject);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
